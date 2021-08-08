b534ab131ea0f10412fec5c76c402bc38b7c6c50
tomasAtrat <tomasatrat@gmail.com>
2021-08-08 18:58:58 -0300

    first commit

---
+++ src/main/java/uy/edu/cei/AmazonCEI/AmazonCeiApplication.java
@@ -0,0 +1,13 @@
+package uy.edu.cei.AmazonCEI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonCeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonCeiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uy/edu/cei/AmazonCEI/AmazonCeiApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.edu.cei.AmazonCEI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonCeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
