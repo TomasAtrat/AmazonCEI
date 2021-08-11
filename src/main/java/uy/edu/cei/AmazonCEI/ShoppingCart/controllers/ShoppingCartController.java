a99b82c2f641a8d750bdc28a45d41c1922be47cc
jose barrera <josebarrera16@outlook.com>
2021-08-10 23:50:32 -0300

    Agregado Soppingcat

--- src/main/java/uy/edu/cei/AmazonCEI/ShoppingCart/controllers/ShoppingCartController.java
+++ src/main/java/uy/edu/cei/AmazonCEI/ShoppingCart/controllers/ShoppingCartController.java
@@ -1,10 +1,34 @@
 package uy.edu.cei.AmazonCEI.ShoppingCart.controllers;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import uy.edu.cei.AmazonCEI.ShoppingCart.clients.IMSClient;
+import uy.edu.cei.AmazonCEI.ShoppingCart.mappers.ShoppingCartMapper;
+import uy.edu.cei.AmazonCEI.common.models.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+@AllArgsConstructor
 public class ShoppingCartController {
+    private final ShoppingCartMapper ShoppingCartMapper;
+    private final IMSClient Client;
+
+    @GetMapping("/{idCarrito}")
+    public List<Item>listShowItem() {
+        final List<UUID> ItemsUUID = this.ShoppingCartMapper.extracUUID();
+
+        final List<Item>ColItem = new ArrayList<>();
+
+        for(UUID uuid:ItemsUUID) {
+            final Item i = this.Client.fetchItem(uuid);
+            ColItem.add(i);
+        }
+        return ColItem;
+        }
 
 
+    }
 
-    
-}
 
 
--- src/main/java/uy/edu/cei/AmazonCEI/ShoppingCart/mappers/ShoppingCartMapper.java
+++ src/main/java/uy/edu/cei/AmazonCEI/ShoppingCart/mappers/ShoppingCartMapper.java
@@ -1,4 +1,17 @@
 package uy.edu.cei.AmazonCEI.ShoppingCart.mappers;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.UUID;
+@Mapper
 public interface ShoppingCartMapper {
+
+
+   @Select("Select Item_id from shopping_cart")
+   List<UUID> extracUUID();
+
+
+
 }
