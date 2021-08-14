f0ae4fee21abdf674b379e7fe18912b275fdabc4
tomasAtrat <tomasatrat@gmail.com>
2021-08-14 18:36:06 -0300

    Unidad lógica de notificación terminada

--- src/main/java/uy/edu/cei/AmazonCEI/Notification/components/NotificationConsumer.java
+++ src/main/java/uy/edu/cei/AmazonCEI/Notification/components/NotificationConsumer.java
@@ -2,10 +2,29 @@ package uy.edu.cei.AmazonCEI.Notification.components;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import uy.edu.cei.AmazonCEI.Notification.services.NotificationService;
+import uy.edu.cei.AmazonCEI.common.messages.CheckoutAction;
+import uy.edu.cei.AmazonCEI.common.messages.CheckoutMessage;
+import uy.edu.cei.AmazonCEI.common.models.Notification;
+
+import static uy.edu.cei.AmazonCEI.Configuration.ActiveMQConfig.CHECKOUT_QUEUE_FOR_NOTIFICATION;
 
 @Component
 @Slf4j
 @AllArgsConstructor
 public class NotificationConsumer {
+
+    private final NotificationService notificationService;
+
+    @JmsListener(destination = CHECKOUT_QUEUE_FOR_NOTIFICATION)
+    public void receiveMessage(@Payload CheckoutMessage payload) {
+        log.info("checkout action: {}", payload);
+        Notification notification = payload.getNotification();
+        if(payload.getAction()== CheckoutAction.SEND_NOTIFICATION){
+            notificationService.send(notification);
+        }
+    }
 }
--- src/main/java/uy/edu/cei/AmazonCEI/Notification/services/NotificationService.java
+++ src/main/java/uy/edu/cei/AmazonCEI/Notification/services/NotificationService.java
@@ -1,7 +1,19 @@
 package uy.edu.cei.AmazonCEI.Notification.services;
 
 import org.springframework.stereotype.Service;
+import uy.edu.cei.AmazonCEI.common.models.ItemToNotificate;
+import uy.edu.cei.AmazonCEI.common.models.Notification;
 
 @Service
 public class NotificationService {
+    public void send(Notification notification) {
+        System.out.println("ID de la notificación: " + notification.getNotification_uuid() + '\n' + "TUS PRODUCTOS");
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("ID Item                   | Nombre      | Precio unitario | Cantidad|");
+        for (ItemToNotificate item:notification.getColItems()) {
+            System.out.println(item.getItem_uuid() + '|' + item.getName() + '|' + item.getCost() + item.getAmount());
+            System.out.println("-------------------------------------------------------------------------");
+        }
+        System.out.println("Precio final: $" + notification.getTotal_cost());
+    }
 }
