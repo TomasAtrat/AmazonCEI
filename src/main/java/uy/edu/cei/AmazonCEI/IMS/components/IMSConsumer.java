5692fac479fe5f6d87fd53c5b85e6e0d2b672b54
AnthonySosa27111999@outlook.com <AnthonySosa27111999@outlook.com>
2021-08-15 21:25:00 -0300

    comiteo cambis en shoppingCart

--- src/main/java/uy/edu/cei/AmazonCEI/IMS/components/IMSConsumer.java
+++ src/main/java/uy/edu/cei/AmazonCEI/IMS/components/IMSConsumer.java
@@ -8,10 +8,12 @@ import org.springframework.stereotype.Component;
 import uy.edu.cei.AmazonCEI.IMS.services.IMSService;
 import uy.edu.cei.AmazonCEI.common.messages.CheckoutAction;
 import uy.edu.cei.AmazonCEI.common.messages.CheckoutMessage;
+import uy.edu.cei.AmazonCEI.common.models.Item;
 import uy.edu.cei.AmazonCEI.common.models.ItemInShoppingCart;
 
 import static uy.edu.cei.AmazonCEI.Configuration.ActiveMQConfig.CHECKOUT_QUEUE_FOR_UPDATE;
-
+import static uy.edu.cei.AmazonCEI.Configuration.ActiveMQConfig.LIST_USER_ITEM;
+import java.util.List;
 @Component
 @Slf4j
 @AllArgsConstructor
@@ -26,4 +28,6 @@ public class IMSConsumer {
             imsService.update(item);
         }
     }
+
+
 }
